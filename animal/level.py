365832587c82e376923ce4f1e7c2b955c69eb9a3
ZhangDongzai <3187480352@qq.com>
2023-05-13 16:52:52 +0800

    分文件, 方便后期维护

--- animal/level.py
+++ animal/level.py
@@ -1,17 +1,15 @@
-class Propertie:
-    name: str
-    restrained_properties: list
-
-
+# 等级类
 class Level:
-    MAX_NUMBER: int
-    UP_EXPERIENCE: int
+    MAX_NUMBER: int = 100       # 最大级数
+    UP_EXPERIENCE: int = 50     # 升第1级需要的经验
 
     def __init__(self) -> None:
+        # 创建实例属性
         self.number = 1
         self.experience = 0
 
     def add(self, experience: int) -> bool:
+        # 增加经验
         self.experience += experience
 
         # 如果: 经验满足升级值 与 当前级数小于最大级数
@@ -21,18 +19,10 @@ class Level:
             self.experience -= Level.UP_EXPERIENCE * self.number
             self.number += 1
 
-            # 升级成功返回 True
+            # 升级返回 True
             return True
         
         else:
-            # 升级未成功返回 False
+            # 未升级返回 False
             return False
-
-        
-class Animal:
-    name: str
-    propertie: Propertie
-
-    def __init__(self) -> None:
-        self.level = Level()
         
---
+++ animal/propertie.py
@@ -0,0 +1,10 @@
+# 属性类
+class Propertie:
+    NAME: str = "propertie"             # 属性名称
+    RESTRAINED_PROPERTIES: list = []    # 克制属性
+    DISSOLVE_PROPERTIES: list = []      # 融合属性
+
+    # BUFF
+    ATTACK: float = 0.0                 # 攻击力
+    DEFENSE: float = 0.0                # 防御力
+    VITALITY: float = 0.0               # 生命力
