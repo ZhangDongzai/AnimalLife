72909c435ddbb0a768aa389c462c7a9eafed1738
ZhangDongzai <3187480352@qq.com>
2023-05-14 15:26:23 +0800

    no

--- animal/animal.py
+++ animal/animal.py
@@ -4,11 +4,26 @@ from propertie import Propertie
 
 # 动物类
 class Animal:
-    NAME = "Animal"                     # 名称
-    PROPERTIM = Propertie()             # 属性
+    NAME = "Animal"             # 名称
+    PROPERTIM = Propertie()     # 属性
 
     def __init__(self) -> None:
 
         # 创建实例属性
-        self.level = Level()            # 等级
-     
+        self.level = Level()    # 等级
+    
+
+if __name__ == "__main__":
+    animal = Animal()
+
+    text = """\
+This is an animal object.
+Its name is {name}.
+Its propertie is {propertie_name}.
+Its level's number is {level_number}.\
+    """.format(
+        name=animal.NAME, 
+        propertie_name=animal.PROPERTIM.NAME, 
+        level_number=animal.level.number)
+    
+    print(text)
